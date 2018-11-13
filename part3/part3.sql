4d792eabb2009594af71b2aa5d2fc458fe559de3
LauraC <celisl2@mailbox.winthrop.edu>
2018-11-13 17:50:47 -0500

    started creating tables and listed order of table inserts

--- part3/part3.sql
+++ part3/part3.sql
@@ -1,1 +1,27 @@
-go to vs code and download the 'SQL Server (mssql)' extention to format sql
+CREATE TABLE User_Account
+    (
+        Id INT UNSIGNED NOT NULL auto_increment,
+        Email VARCHAR(256) NOT NULL UNIQUE,
+        FirstName VARCHAR(255) NOT NULL,
+        LastNAme VARCHAR(255) NOT NULL,
+        Phone TINYINT UNSIGNED NOT NULL,
+        StreetAddress VARCHAR(256) NOT NULL,
+        City VARCHAR(255) NOT NULL,
+        Zip MEDIUMINT UNSIGNED NOT NULL,
+        Salt CHAR(10) NOT NULL,
+        Hash CHAR(40) NOT NULL,
+        PRIMARY KEY (Id)
+    );
+-- subscription_type table here
+CREATE TABLE Payment_Method
+    (
+
+    );
+-- invoice table here
+-- profile table here
+-- movie table here
+-- genre table here
+-- type_of table here
+-- views_mobie table here
+-- person table here
+-- directed_by_mov table here
