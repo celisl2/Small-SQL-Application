79e151b2365bb9df3fc5cd4be4a0edbca3edbf06
LauraC <celisl2@mailbox.winthrop.edu>
2018-11-16 10:46:33 -0500

    finished inserts and all tables

--- part3/part3.sql
+++ part3/part3.sql
@@ -47,8 +47,8 @@ CREATE TABLE Genre
     );
 CREATE TABLE Type_Of
     (
-        MovieID INT UNSIGNED NOT NULL,
-        GenreID INT UNSIGNED NOT NULL,
+        MovieID INT UNSIGNED,
+        GenreID INT UNSIGNED,
         PRIMARY KEY (MovieID, GenreID),
         FOREIGN KEY (MovieID) REFERENCES Movie(Id) ON UPDATE CASCADE ON DELETE CASCADE,
         FOREIGN KEY (GenreID) REFERENCES Genre(Id) ON UPDATE CASCADE ON DELETE CASCADE
@@ -57,16 +57,73 @@ CREATE TABLE Type_Of
 -- directed_by_mov table here = kim
 -- INSERTS GO HERE
 INSERT INTO User_Account (Id, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
-VALUES (NULL, "emmekemammo-4655@yopmail.com", "Kieran", "Moses", "7632135215", "1069  Cherry Tree Drive", "Jacksonville", "FL", "32204", "9c8ffcdf2b", SHA1("9c8ffcdf2bpassword"));
+VALUES (NULL, "emmekemammo-4655@yopmail.com", "Kieran", "Moses", "7632135215", "1069  Cherry Tree Drive", "Jacksonville", "FL", 32204, "9c8ffcdf2b", SHA1("9c8ffcdf2bpassword"));
 INSERT INTO User_Account (Id, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
-VALUES (NULL, "ulatr@laparbgt.ga", "Dianne", "Altreche", "2706015202", "2982 Suscipit Ave","Galzignano Terme","Veneto","RT9 6CM", "vu2tdahq2m" , SHA1("vu2tdahq2mpassword"));
+VALUES (NULL, "ulatr@laparbgt.ga", "Dianne", "Altreche", "2706015202", "2982 Suscipit Ave","Galzignano Terme","Veneto",28277, "vu2tdahq2m" , SHA1("vu2tdahq2mpassword"));
 INSERT INTO User_Account (Id, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
-VALUES (NULL, "babde-1@bohani.tk", "Noe", "Freelon", "3876308409", "Ap #742-354 Phasellus St.","Salamanca","CL","33800-094", "tg7gpln2j2", SHA1("tg7gpln2j2password"));
+VALUES (NULL, "babde-1@bohani.tk", "Noe", "Freelon", "3876308409", "Ap #742-354 Phasellus St.","Salamanca","CL",29715, "tg7gpln2j2", SHA1("tg7gpln2j2password"));
 INSERT INTO User_Account (ID, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
-VALUES (NULL, "TheFloweryGamer@gmail.com", "Bob", "Claswell", "7025197831", "440-1535 Enim, Av.","Benalla","Victoria","14574", "7cs634v0e4", SHA1("7cs634v0e4password"));
+VALUES (NULL, "TheFloweryGamer@gmail.com", "Bob", "Claswell", "7025197831", "440-1535 Enim, Av.","Benalla","Victoria",14574, "7cs634v0e4", SHA1("7cs634v0e4password"));
 INSERT INTO User_Account (Id, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
-VALUES (NULL, "ziiasxdb@imgof.com", "Tahani", "Moe", "8164589215", "Ap #735-4347 Placerat. Road","Pontevedra","GA","4761", "5mcjzx4pjd", SHA1("5mcjzx4pjdpassword"));
+VALUES (NULL, "ziiasxdb@imgof.com", "Tahani", "Moe", "8164589215", "Ap #735-4347 Placerat. Road","Pontevedra","GA",4761, "5mcjzx4pjd", SHA1("5mcjzx4pjdpassword"));
 INSERT INTO User_Account (Id, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
-VALUES (NULL, "zoopjcym@abyssmail.com", "Chidi", "Ngani", "9178835792", "P.O. Box 130, 4895 In, St.","St. Petersburg","FL","78757", "ndenf7xy9b", SHA1("ndenf7xy9bpassword"));
+VALUES (NULL, "zoopjcym@abyssmail.com", "Chidi", "Ngani", "9178835792", "P.O. Box 130, 4895 In, St.","St. Petersburg","FL",78757, "ndenf7xy9b", SHA1("ndenf7xy9bpassword"));
 INSERT INTO User_Account (Id, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
 VALUES (NULL, "zdixskyj@boximail.com", "Lesley", "Knope", "5012398162", "8749 Rutrum. Avenue","Kraków","MP","1570", "vxhcujlh6d", SHA1("vxhcujlh6dpassword"));
+
+
+INSERT INTO Subscription_Type (Id, Name, Screens, Price, Description)
+VALUES (NULL, "Standard", 2, 10.99, "Two screens with HD and downloads");
+INSERT INTO Subscription_Type (Id, Name, Screens, Price, Description)
+VALUES (NULL, "Basic", 1, 7.99, "One screen basic");
+INSERT INTO Subscription_Type (Id, Name, Screens, Price, Description)
+VALUES (NULL, "Premium", 4, 13.99, "4 screens with Ultra HD and downloads");
+
+
+INSERT INTO Payment_Method (Id, CreditCardNumber, CVV, ExpirationDate, AccID)
+VALUES (NULL, "4790660089521535", "884", 20230501, 1);
+INSERT INTO Payment_Method (Id, CreditCardNumber, CVV, ExpirationDate, AccID)
+VALUES (NULL, "5528379801057043", "063", 20260401, 2);
+INSERT INTO Payment_Method (Id, CreditCardNumber, CVV, ExpirationDate, AccID)
+VALUES (NULL, "5038616215469295", "130", 20281201, 3);
+INSERT INTO Payment_Method (Id, CreditCardNumber, CVV, ExpirationDate, AccID)
+VALUES (NULL, "348770964799265", "8705", 20280401, 4);
+INSERT INTO Payment_Method (Id, CreditCardNumber, CVV, ExpirationDate, AccID)
+VALUES (NULL, "6011175300693334", "442", 20270301, 5);
+INSERT INTO Payment_Method (Id, CreditCardNumber, CVV, ExpirationDate, AccID)
+VALUES (NULL, "6211175300693334", "442", 20270301, 6);
+INSERT INTO Payment_Method (Id, CreditCardNumber, CVV, ExpirationDate, AccID)
+VALUES (NULL, "5028379801057043", "063", 20260401, 7);
+-- can do all users with the same credit card expiration date
+
+INSERT INTO Genre (Id, Genre)
+VALUES(NULL, "Horror");
+INSERT INTO Genre (Id, Genre)
+VALUES(NULL, "Comedy");
+INSERT INTO Genre (Id, Genre)
+VALUES(NULL, "Romantic");
+INSERT INTO Genre (Id, Genre)
+VALUES(NULL, "Sci-Fi");
+INSERT INTO Genre (Id, Genre)
+VALUES(NULL, "LGTBQ");
+INSERT INTO Genre (Id, Genre)
+VALUES(NULL, "Thriller");
+INSERT INTO Genre (Id, Genre)
+VALUES(NULL, "Adventure");
+
+INSERT INTO Type_Of (MovieID, GenreID)
+VALUES (NULL, NULL);
+INSERT INTO Type_Of (MovieID, GenreID)
+VALUES (NULL, NULL);
+INSERT INTO Type_Of (MovieID, GenreID)
+VALUES (NULL, NULL);
+INSERT INTO Type_Of (MovieID, GenreID)
+VALUES (NULL, NULL);
+INSERT INTO Type_Of (MovieID, GenreID)
+VALUES (NULL, NULL);
+INSERT INTO Type_Of (MovieID, GenreID)
+VALUES (NULL, NULL);
+INSERT INTO Type_Of (MovieID, GenreID)
+VALUES (NULL, NULL);
+
+--can do a join on people have subscription types greater than $10 and live in florida
