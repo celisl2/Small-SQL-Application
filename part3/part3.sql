e4facfb0a92b8a192ddaf3716057e7d467697c92
LauraC <celisl2@mailbox.winthrop.edu>
2018-11-18 18:37:50 -0500

    finished joins

--- part3/part3.sql
+++ part3/part3.sql
@@ -41,7 +41,7 @@ CREATE TABLE Invoice
         AccID INT UNSIGNED NOT NULL,
         SubID INT UNSIGNED NOT NULL,
         PaymentID INT UNSIGNED NOT NULL,
-        PurchaseAmount DECIMAL (4, 2) NOT NULL,
+        PurchaseAmount DECIMAL (5, 2) NOT NULL,
         PRIMARY KEY (Id),
         FOREIGN KEY (AccID) REFERENCES User_Account(Id) ON UPDATE CASCADE ON DELETE CASCADE,
         FOREIGN KEY (SubID) REFERENCES Subscription_Type(Id) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -84,7 +84,7 @@ CREATE TABLE Views_Movie
     (
         MovieID INT UNSIGNED,
         ProfileID INT UNSIGNED,
-        PercentComplete DECIMAL (5, 2) NOT NULL,
+        PercentComplete DECIMAL (4, 2) NOT NULL,
         PRIMARY KEY (MovieID, ProfileID),
         FOREIGN KEY (MovieID) REFERENCES Movie(Id) ON UPDATE CASCADE ON DELETE CASCADE,
         FOREIGN KEY (ProfileID) REFERENCES Profile(Id) ON UPDATE CASCADE ON DELETE CASCADE
@@ -108,7 +108,7 @@ CREATE TABLE Directed_By_Mov
 
 -- INSERTS GO HERE
 INSERT INTO User_Account (Id, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
-VALUES (NULL, "emmekemammo-4655@yopmail.com", "Kieran", "Moses", "7632135215", "1069  Cherry Tree Drive", "Jacksonville", "FL", 32204, "9c8ffcdf2b", SHA1("9c8ffcdf2bpassword"));
+VALUES (NULL, "emmekemammo-4655@yopmail.com", "Kieran", "Moses", "7632135215", "1069  Cherry Tree Drive", "Jacksonville", "NC", 32204, "9c8ffcdf2b", SHA1("9c8ffcdf2bpassword"));
 INSERT INTO User_Account (Id, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
 VALUES (NULL, "ulatr@laparbgt.ga", "Dianne", "Altreche", "2706015202", "2982 Suscipit Ave","Galzignano Terme","Veneto",28277, "vu2tdahq2m" , SHA1("vu2tdahq2mpassword"));
 INSERT INTO User_Account (Id, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
@@ -116,7 +116,7 @@ VALUES (NULL, "babde-1@bohani.tk", "Noe", "Freelon", "3876308409", "Ap #742-354 
 INSERT INTO User_Account (ID, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
 VALUES (NULL, "TheFloweryGamer@gmail.com", "Bob", "Claswell", "7025197831", "440-1535 Enim, Av.","Benalla","Victoria",14574, "7cs634v0e4", SHA1("7cs634v0e4password"));
 INSERT INTO User_Account (Id, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
-VALUES (NULL, "ziiasxdb@imgof.com", "Tahani", "Moe", "8164589215", "Ap #735-4347 Placerat. Road","Pontevedra","GA",4761, "5mcjzx4pjd", SHA1("5mcjzx4pjdpassword"));
+VALUES (NULL, "ziiasxdb@imgof.com", "Tahani", "Moe", "8164589215", "Ap #735-4347 Placerat. Road","Pontevedra","FL",4761, "5mcjzx4pjd", SHA1("5mcjzx4pjdpassword"));
 INSERT INTO User_Account (Id, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
 VALUES (NULL, "zoopjcym@abyssmail.com", "Chidi", "Ngani", "9178835792", "P.O. Box 130, 4895 In, St.","St. Petersburg","FL",78757, "ndenf7xy9b", SHA1("ndenf7xy9bpassword"));
 INSERT INTO User_Account (Id, Email, FirstName, LastName, Phone, StreetAddress, City, State, Zip, Salt, Hash)
@@ -273,7 +273,19 @@ INSERT INTO Directed_By_Mov (MovieID, PersonID)
 VALUES (6, 8);
 INSERT INTO Directed_By_Mov (MovieID, PersonID)
 VALUES (7, 9);                                                                                                                                       
---can do a join on people have subscription types greater than $10 and live in florida
+-- Question: Give the first names of the people who have paid more than 10.00
+SELECT User_Account.FirstName, Invoice.PurchaseAmount
+FROM Invoice RIGHT JOIN User_Account ON Invoice.AccID = User_Account.Id WHERE Invoice.PurchaseAmount > 10.00;
+-- Question: How many people have paid more than 10.00 and live in Florida
+SELECT count(*), User_Account.State, Invoice.PurchaseAmount
+FROM Invoice RIGHT JOIN User_Account ON Invoice.AccID = User_Account.Id AND User_Account.State = "FL" 
+WHERE Invoice.PurchaseAmount > 10.00
+GROUP BY User_Account.State, Invoice.PurchaseAmount;
+-- Question: name the cities of all users that share the same expiration date ordered alphabetically
+SELECT User_Account.City, Payment_Method.ExpirationDate FROM User_Account RIGHT JOIN Payment_Method
+ON User_Account.Id = Payment_Method.AccID
+INNER JOIN (SELECT ExpirationDate, count(*) FROM Payment_Method GROUP BY ExpirationDate HAVING count(*) > 1) AS T
+ON Payment_Method.ExpirationDate = T.ExpirationDate ORDER BY User_Account.City ASC;
+
 -- People that have viewed the same movie
--- Movie that has 2 directors        
--- can do all users with the same credit card expiration date                                                                                                                              
+-- Movie that has 2 directors 
